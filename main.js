cd25c9447795b8e9b0a0490601a3593aa9c18a32
ddongni <de0309@naver.com>
2021-07-13 11:36:59 +0900

    라우터 사용하여 코드 복잡도 낮추기

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-var express = require('express')
-var app = express()
-var port = 3000
+var express = require('express');
+var app = express();
+var port = 3000;
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
 
 //public 디렉토리에서 찾음
 app.use(express.static('public'));
@@ -25,134 +23,8 @@ app.get('*', function(request, response, next){
   });
 });
 
-/* app.get('/', (req, res) => {
-  res.send('Hello World!')
-})*/
-app.get('/',function(request, response){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}
-      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-      `,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
-})
-
-app.get('/page/:pageId', function(request,response, next){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      if(err){
-        next(err);  
-      } else {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(request.list);
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        );
-        response.send(html);
-      }
-    });
-});
-
-app.get('/create', function(request,response){
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-});
-
-app.post('/create_process', function(request, response){
-  /*
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
-  });
-  */
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.writeHead(302, {Location: `/?id=${title}`});
-    response.end();
-  });
-});
-
-app.get('/update/:pageId',function(request,response){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(request.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
-});
-
-app.post('/update_process', function(request, response){
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/`);
-    });
-  });
-});
-
-app.post('/delete_process',function(request,response){
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  });
-});
+app.use('/',indexRouter);
+app.use('/topic', topicRouter); // /topic 으로 시작하는 주소들에게 topicRouter 미들웨어 적용 
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
